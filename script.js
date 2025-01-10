486984f674adaa8d47971efaf044242dbbb40ac1
Nate <187714607+Aethrynn@users.noreply.github.com>
2025-01-10 16:43:35 -0500

    created html and js files. Added pseudocode for getComputerChoice

---
+++ script.js
@@ -0,0 +1,7 @@
+function getComputerChoice(){
+    // Create an array with all 3 possible results
+    const computerChoices = ["rock", "paper", "scissors"];
+    // Randomly choose a number betweeen 1 and 3
+    // Get the value from the array at the index of the random number
+    // Return the chosen value
+}
