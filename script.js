536f14b46797fe51ad382a1d9127f8653362e44d
Nate <187714607+Aethrynn@users.noreply.github.com>
2025-01-23 14:28:28 -0500

    wrote getComputerChoice function

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function getComputerChoice(){
     // Create an array with all 3 possible results
     const computerChoices = ["rock", "paper", "scissors"];
+
     // Randomly choose a number betweeen 1 and 3
+    let randomChoice = Math.floor(Math.random() * 3);
+
     // Get the value from the array at the index of the random number
+    let rpsAction = computerChoices[randomChoice];
+
     // Return the chosen value
+    console.log(rpsAction);
 }
